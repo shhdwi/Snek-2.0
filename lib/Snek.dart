318b5ce9f871d6e87e1db22e3c635b7465361bb5
Shrish Dwivedi <72087189+shhdwi@users.noreply.github.com>
2022-02-15 21:16:30 +0530

    gg done

--- lib/Snek.dart
+++ lib/Snek.dart
@@ -15,15 +15,24 @@ class _MainScreenState extends State<MainScreen> {
   static List<int> snakePosition =[45,65,85,105,125];
 
   int numberOfSquares= 760;
+  Duration duration = Duration(milliseconds:50);
+  int _selectedColor=0;
   static var randomNumber =Random();
   int food =randomNumber.nextInt(700);
   void generateNewFood(){
-    food = randomNumber.nextInt(700);
+    int food1= randomNumber.nextInt(700);
+    while(snakePosition.contains(food1)){
+      food1= randomNumber.nextInt(700);
+
+
+    }
+    food =food1;
+
   }
 
   void startGame(){
     snakePosition =[45,65,85,105,125];
-    const duration = const Duration(milliseconds: 300);
+    const duration = const Duration(milliseconds: 50);
     Timer.periodic(duration, (Timer timer) {
       updateSnake();
       if (gameOver()){
@@ -75,6 +84,11 @@ class _MainScreenState extends State<MainScreen> {
 
       if (snakePosition.last ==food){
         generateNewFood();
+        setState(() {
+
+          duration=Duration(milliseconds: 350- 10*(snakePosition.length-5));
+        });
+
 
       }else{
         snakePosition.removeAt(0);
@@ -117,83 +131,147 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        children: <Widget>[
-          Expanded(
-              child: GestureDetector(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: GestureDetector(
 
-                onVerticalDragUpdate: (details){
-                  if(direction != 'up' && details.delta.dy >0 ){
-                    direction ='down';
-                  }else if (direction!='down'&& details.delta.dy<0){
-                    direction ='up';
+                  onVerticalDragUpdate: (details){
+                    if(direction != 'up' && details.delta.dy >0 ){
+                      direction ='down';
+                    }else if (direction!='down'&& details.delta.dy<0){
+                      direction ='up';
 
-                  }
-                },
-                onHorizontalDragUpdate: (details){
-                  if(direction !='left' && details.delta.dx>0){
-                    direction='right';
+                    }
+                  },
+                  onHorizontalDragUpdate: (details){
+                    if(direction !='left' && details.delta.dx>0){
+                      direction='right';
 
-                  } else if (direction!='right' && details.delta.dx <0){
-                    direction ='left';
-                  }
-                },
-
-
-                child: Container(
-                  child: GridView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                      itemCount: numberOfSquares,
+                    } else if (direction!='right' && details.delta.dx <0){
+                      direction ='left';
+                    }
+                  },
 
 
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 20),
-                      itemBuilder:(BuildContext context, int index){
-                      if (snakePosition.contains(index)){
-                        return Center(
-                          child: Container(
+                  child: Container(
+                    child: GridView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                        itemCount: numberOfSquares,
+
+
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 20),
+                        itemBuilder:(BuildContext context, int index){
+                        if (snakePosition.contains(index)){
+                          return Center(
+                            child: Container(
+                              padding: EdgeInsets.all(2),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(5),
+                                child: Container(color: getColor(_selectedColor),),
+                              ),
+                            ),
+
+                          );
+                        }
+                        if (index==food){
+                          return Container(
                             padding: EdgeInsets.all(2),
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(5),
-                              child: Container(color: Colors.white,),
+                              child: Container(color: Colors.green,),
+
+
+
+                            ),
+                          );
+                        }
+                        else{
+                          return Container(
+                            padding: EdgeInsets.all(2),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(5),
+                              child: Container(color: Colors.grey[900],),
+
+                            ),
+                          );
+                        }
+                        }
+                    ),
+                  ),
+
+                )
+
+            ),
+
+            Padding(padding: EdgeInsets.only(bottom: 20,left: 20,right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: startGame,
+                    child: Text("S T A R T", style: TextStyle(color: Colors.white,fontSize: 20),),
+
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Color:   ",
+                          style: TextStyle(color: Colors.white,fontSize: 20)),
+                      SizedBox(height: 8,),
+                      Wrap(
+                        children: List<Widget>.generate(4, (int index) => GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              _selectedColor=index;
+                            });
+
+
+                          },
+
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: CircleAvatar(
+                              radius: 14,
+                              backgroundColor: index==0?Colors.white:index==1?Colors.greenAccent:index==2?Colors.redAccent:Colors.orangeAccent,
+                              child: _selectedColor==index?Icon(Icons.done,
+                                color: Colors.black,
+                                size: 16,):Container(),
+
                             ),
                           ),
+                        )),
+                      )
+                    ],
+                  )
 
-                        );
-                      }
-                      if (index==food){
-                        return Container(
-                          padding: EdgeInsets.all(2),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(5),
-                            child: Container(color: Colors.green,),
+                ],
+              ),
 
 
-
-                          ),
-                        );
-                      }
-                      else{
-                        return Container(
-                          padding: EdgeInsets.all(2),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(5),
-                            child: Container(color: Colors.grey[900],),
-
-                          ),
-                        );
-                      }
-                      }
-                  ),
-                ),
-
-              )
-
-          ),
-
-        ],
+            )],
+        ),
       ),
     );
+  }
+  Color getColor(int _selectedColor){
+    if (_selectedColor==2){
+      return Colors.redAccent;
+    }
+    else if (_selectedColor==1){
+      return Colors.greenAccent;
+
+
+    }
+    if (_selectedColor==3){
+      return Colors.orangeAccent;
+
+    }
+    else{
+      return Colors.white;
+    }
   }
 }
